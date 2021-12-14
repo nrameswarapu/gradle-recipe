1dd141e66ecc4d67a93ffb3b95fb18025643de8d
nrameswarapu <rameswn@gmail.com>
2021-12-14 18:22:36 -0500

    Add files via upload

---
+++ src/test/java/org/openrewrite/samples/FindReplaceTest.kt
@@ -0,0 +1,24 @@
+package org.openrewrite.samples
+
+import org.openrewrite.java.JavaRecipeTest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions
+
+class FindReplaceTest: JavaRecipeTest {
+	@Test
+	fun replaceText() = assertChanged(
+		recipe = FindReplaceText("Hi","Hello"),
+		before = """
+			package com.naga;
+			class A {
+				String hello = "Hi";
+			}
+		"""",
+		after = """
+			package com.naga;
+			class A {
+				String hello = "Hello";
+			}
+		""""
+	)
+}
